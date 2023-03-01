99cdb6e3d09b4dfba381159075a9931b388efc35
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-03-01 19:45:35 +0100

    First version of php script

---
+++ load.php
@@ -0,0 +1,23 @@
+<?php
+    include 'conn.php';
+    $type = $_GET['type'];
+    $data = array();
+    //Make query
+    if($type == 'item'){
+        $query = "SELECT name, barcode, category, storage FROM products;";
+    }
+    if($type == 'category'){
+        $query = "SELECT `name` FROM `categories`";
+    }
+    if($type == 'storage'){
+        $query = "SELECT `name` FROM `storages`";
+    }
+    //Get data from database
+    $result = mysqli_query($conn, $query);
+    if ($result->num_rows > 0) {
+        while($answer = mysqli_fetch_assoc($result)){
+            $data[] = $answer;
+        }
+    }
+    echo json_encode($data);
+?>
