0df29b6f9958aa30fef930e52a23f086c504084e
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-03-07 22:10:59 +0100

    First version, scripts that allows to add data, storages, categories and to fetch data (storages, categories), that can be choosen in item adding page

---
+++ jsjson.js
@@ -0,0 +1,101 @@
+function addItem(){
+  if(document.getElementById("name").value.length == 0){
+    alert("Please insert some value into `name` field!");
+  }else{
+      //preparing JSON
+      var formData = {
+        type: "item",
+        name: document.getElementById("name").value,
+        barcode: document.getElementById("barcode").value,
+        category: document.getElementById("category").value,
+        storage: document.getElementById("storage").value
+      };
+      var jsonData = JSON.stringify(formData);
+      // code to send the jsonData to a server-side script
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("POST","insert.php");
+      xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
+      xmlhttp.send(jsonData);
+      //Reset form inputs
+      document.getElementById("data").reset();
+  }
+}
+function addCategory(){
+  if(document.getElementById("name").value.length == 0){
+    alert("Please insert some value into `Category name` field!");
+  }else{
+      //preparing JSON
+      var formData = {
+        type: "category",
+        name: document.getElementById("name").value,
+      };
+      var jsonData = JSON.stringify(formData);
+      // code to send the jsonData to a server-side script
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("POST","insert.php");
+      xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
+      xmlhttp.send(jsonData);
+      //Reset form inputs
+      document.getElementById("data").reset();
+  }
+  if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+    alert(xmlhttp.responseText);
+}
+}
+function addStorage(){
+  if(document.getElementById("name").value.length == 0){
+    alert("Please insert some value into `Storage name` field!");
+  }else{
+      //preparing JSON
+      var formData = {
+        type: "storage",
+        name: document.getElementById("name").value,
+      };
+      var jsonData = JSON.stringify(formData);
+      // code to send the jsonData to a server-side script
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("POST","insert.php");
+      xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
+      xmlhttp.send(jsonData);
+      //Reset form inputs
+      document.getElementById("data").reset();
+      //get the response from the php script
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          let response = xmlhttp.responseText;
+          alert(response);
+        }
+      }
+  }
+}
+function fetchOptionsData(){
+  fetch("load.php?type=category")
+        .then(function (response) {
+            if (response.status >= 200 && response.status < 300) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(function (data) {
+          listOptions(data, category);
+        });
+    fetch("load.php?type=storage")
+      .then(function (response) {
+          if (response.status >= 200 && response.status < 300) {
+              return response.json();
+          }
+          throw new Error(response.statusText);
+      })
+      .then(function (data) {
+          listOptions(data, storage);
+      });                   
+}
+function listOptions(data, id){
+  var length = data.length;
+    for (var i = 0; i<length; i++){
+      var option = document.createElement('option');
+      option.value = data[i].name;
+      option.innerHTML = data[i].name;
+      id.appendChild(option);
+  }
+}
