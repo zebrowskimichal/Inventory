e210d085c92306d49082939899ea2f38101919f1
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-03-10 22:24:14 +0100

    repaired all id names in script (all to ->id)

--- stats.php
+++ stats.php
@@ -2,16 +2,16 @@
     include 'conn.php';
     $stats = array();
     //Get Total Elements-number of products from db
-        $question = mysqli_query($conn, "SELECT COUNT(productId) AS numberOfProducts FROM products;");
+        $question = mysqli_query($conn, "SELECT COUNT(id) AS numberOfProducts FROM products;");
         //$stats += 'numberOfProducts' = mysqli_fetch_row($question);
         $stats[] = array("numberOfProducts"=>mysqli_fetch_row($question)[0]);
 
     //Get total-storages numbers
-        $question = mysqli_query($conn, "SELECT COUNT(storageId) AS numberOfStorages FROM storages;");
+        $question = mysqli_query($conn, "SELECT COUNT(id) AS numberOfStorages FROM storages;");
         $stats[] = array("totalStorages"=>mysqli_fetch_row($question)[0]);
     
     //Get total-categories number
-        $question = mysqli_query($conn, "SELECT COUNT(categoryId) AS numberOfCategories FROM categories;");
+        $question = mysqli_query($conn, "SELECT COUNT(id) AS numberOfCategories FROM categories;");
         $stats[] = array("totalCategories"=>mysqli_fetch_row($question)[0]);
 
 /*
