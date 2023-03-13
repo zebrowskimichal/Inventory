aee866858cce2230e9d689cf03490ece9aa45843
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-03-13 22:09:04 +0100

    Added function changeItem(), which sends data to update.php and updates item. Also added funtion deleteElement(), which deletes Element

--- js/jsjson.js
+++ js/jsjson.js
@@ -1,33 +1,11 @@
-function addItem(){
-  if(document.getElementById("name").value.length == 0){
-    alert("Please insert some value into `name` field!");
-  }else{
-      //preparing JSON
-      var formData = {
-        type: "item",
-        name: document.getElementById("name").value,
-        barcode: document.getElementById("barcode").value,
-        category: document.getElementById("category").value,
-        storage: document.getElementById("storage").value
-      };
-      var jsonData = JSON.stringify(formData);
-      // code to send the jsonData to a server-side script
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.open("POST","insert.php");
-      xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
-      xmlhttp.send(jsonData);
-      //Reset form inputs
-      document.getElementById("data").reset();
-  }
-}
 function addValue(valueType){
   if(document.getElementById("name").value.length == 0){
     alert("Please insert some value into `Category name` field!");
   }else{
-    /*
-    if(valueType == "item"){
-    var formData = {
-        type: "item",
+    //preparing JSON
+    if(valueType == 'item'){
+      var formData = {
+        type: valueType,
         name: document.getElementById("name").value,
         barcode: document.getElementById("barcode").value,
         category: document.getElementById("category").value,
@@ -39,16 +17,10 @@ function addValue(valueType){
         name: document.getElementById("name").value,
       };
     }
-    */
-      //preparing JSON
-      var formData = {
-        type: valueType,
-        name: document.getElementById("name").value,
-      };
       var jsonData = JSON.stringify(formData);
       // code to send the jsonData to a server-side script
       var xmlhttp = new XMLHttpRequest();
-      xmlhttp.open("POST","insert.php");
+      xmlhttp.open("POST","php/insert.php");
       xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
       xmlhttp.send(jsonData);
       //Reset form inputs
@@ -62,8 +34,9 @@ function addValue(valueType){
       }
   }
 }
+//Script that gets data: storages, categories from server
 function fetchOptionsData(){
-  fetch("load.php?type=category")
+  fetch("php/load.php?type=category")
         .then(function (response) {
             if (response.status >= 200 && response.status < 300) {
                 return response.json();
@@ -73,7 +46,7 @@ function fetchOptionsData(){
         .then(function (data) {
           listOptions(data, category);
         });
-    fetch("load.php?type=storage")
+    fetch("php/load.php?type=storage")
       .then(function (response) {
           if (response.status >= 200 && response.status < 300) {
               return response.json();
@@ -84,6 +57,7 @@ function fetchOptionsData(){
           listOptions(data, storage);
       });                   
 }
+//Script that create <select> options: storages, categories
 function listOptions(data, id){
   var length = data.length;
     for (var i = 0; i<length; i++){
@@ -92,4 +66,58 @@ function listOptions(data, id){
       option.innerHTML = data[i].name;
       id.appendChild(option);
   }
+}
+//Functions sands data to update.php and updates item
+function changeItem(valueType, valueId){
+  if(valueType == 'item'){
+    var formData = {
+      type: valueType,
+      id: valueId,
+      name: document.getElementById("name").value,
+      barcode: document.getElementById("barcode").value,
+      category: document.getElementById("category").value,
+      storage: document.getElementById("storage").value
+    };  
+  } else {
+    var formData = {
+      type: valueType,
+      id: valueId,
+      name: document.getElementById("name").value,
+    };
+  }
+  var updateData = JSON.stringify(formData);
+  // code to send the jsonData to a server-side script
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST","php/update.php?");
+  xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
+  xmlhttp.send(updateData);
+  //get the response from the php script
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      let response = xmlhttp.responseText;
+      alert(response);
+      backEdit();
+      pageLoad();
+    }
+  }
+}
+//function that deletes element
+function deleteElement(valueId, valueType){
+  if(confirm("Do you really want to delete this " + valueType + " ?")){
+    fetch("php/delete.php?type=" + valueType + "&id=" + valueId)
+    .then(function (response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response.text();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(function (response) {
+      alert(response);
+      items.innerHTML = ""
+      pageLoad();
+    });
+  } else {
+    alert("nie usuwam!");
+  }
+
 }
