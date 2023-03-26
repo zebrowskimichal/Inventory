ce7d360af3ea782282fe19200ffbc91277474740
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-03-26 21:50:37 +0200

    changed get form to post form which sends json data to this script

--- php/login.php
+++ php/login.php
@@ -1,17 +1,22 @@
 <?php
     include 'conn.php';
-    $username = $_GET['login'];
-    $password = $_GET['password'];
+     //get and decode JSON input
+    header("Content-Type: application/json");
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+    $username = $data['username'];
+    $password = $data['password'];
     //Script logins user to page
-    $query = "SELECT * FROM users where username = ".$username." AND password = ".$password.";";
+    $query = "SELECT * FROM users where username = ' ".$username." ' AND password = ' ".$password." ';";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
     $count = mysqli_num_rows($result);  
-
+    //check if user credentials are correct
     if($count == 1){  
+        //create a session
         $_SESSION['username'] = $username;
         $_SESSION['password'] = $password;
-        echo "Login succesfull!";
+        echo "Login succesful";
     }else{  
         echo "Login failed!";
     }
